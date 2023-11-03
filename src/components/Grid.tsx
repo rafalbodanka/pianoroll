bee522ec54955dc6932b55d2f00219aef89b425c
Rafał - laptop <rafal.bodanka@gmail.com>
2023-11-03 15:04:41 +0100

    add svg icons for player settings and back button

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -25,12 +25,19 @@ export default function Grid({
     }
 
     return (
-        <div className={`${playedRollIndex !== -1 && 'grid grid-cols-5'} py-4 px-[3em]`}>
+        <div className={`${playedRollIndex !== -1 && 'grid grid-cols-5'} lg:mt-4 py-4 px-[3em]`}>
             <div className={`col-span-5 lg:col-span-4 ${playedRollIndex === -1 ? 'hidden' : ''}`}>
-                <div className="cursor-pointer mb-8 inline-flex select-none bg-[#154151] tracking-wide text-[#F2F2F2] text-sm font-bold rounded-md py-1 px-2" onClick={() => { setPlayedRollIndex(-1) }}>&larr; back </div>
+                <div
+                className="cursor-pointer mb-8 inline-flex select-none bg-[#154151] tracking-wide text-[#F2F2F2] text-sm font-bold rounded-md py-1 px-2"
+                onClick={() => { setPlayedRollIndex(-1) }}>
+                    <div className="flex items-center gap-1">
+                        <img className="h-4" src={'img/arrow-left.svg'} alt='arrow-left'/>
+                        <span>back</span> 
+                    </div>
+                    </div>
                 {playedRollIndex !== -1 && <Player playedRollIndex={playedRollIndex} partData={partData} />}
             </div>
-            <div className={`${playedRollIndex === -1 ? 'main-grid' : 'mt-20 col-span-5 lg:col-span-1 flex flex-col gap-8 lg:overflow-y-scroll py-4 lg:h-[calc(100vh-44px-3em-80px)]'}`}>
+            <div className={`${playedRollIndex === -1 ? 'main-grid' : 'mt-20 lg:mt-0 col-span-5 lg:col-span-1 flex flex-col gap-8 lg:overflow-y-scroll py-4 lg:h-[calc(100vh-28px-3em)]'}`}>
                 {!isLoading ?
                     Array.from({ length: playedRollIndex === -1 ? 20 : 19 }, (_, it) => {
                         if (playedRollIndex === it) return
--- src/components/PlayerOptions.tsx
+++ src/components/PlayerOptions.tsx
@@ -8,12 +8,25 @@ export default function PlayerOptions({
         setIsPlaying(prev => !prev)
     }
 
+    const stopAction = () => {
+        setIsPlaying(false)
+    }
+
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center gap-4">
             <div
                 onClick={switchPlay}
-                className="mt-6 cursor-pointer bg-gray-300 inline-flex items-center justify-center h-8 w-8" >
-                <div className="select-none" dangerouslySetInnerHTML={{ __html: isPlaying ? '&#8214;' : '&#10148;' }}></div>
+                className="mt-6 cursor-pointer bg-gray-300 hover:bg-gray-400 inline-flex items-center justify-center h-8 w-8" >
+                {isPlaying ?
+                    <img className="h-4" src={'img/pause.svg'} />
+                    :
+                    <img className="h-4" src={'img/play.svg'} />
+                }
+            </div>
+            <div
+                onClick={stopAction}
+                className="mt-6 cursor-pointer bg-gray-300 hover:bg-gray-400 inline-flex items-center justify-center h-8 w-8" >
+                <img className="h-4" src={'img/stop.svg'} />
             </div>
         </div>
     )
